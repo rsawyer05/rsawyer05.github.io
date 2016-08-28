f0389a17068a419878f2afb26bd8e1af8d8746af
Russell Sawyer <rsawyer05@yahoo.com>
2016-08-27 19:11:09 -0500

    Added Lightbox styles

---
+++ techdegree/proj4/js/app.js
@@ -0,0 +1,47 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+var $btnPrev = $('<button id="btnPrev"> < </button>');
+var $btnNext = $('<button id="btnNext"> > </button>')
+var $btnExit = $('<button id="btnExit"> x </button>')
+//Add image to overlay
+$overlay.append($image);
+//Add caption to overlay
+$overlay.append($caption);
+//Add left button to overlay
+$overlay.append($btnPrev);
+//Add right button to overlay
+$overlay.append($btnNext);
+//Add exit button to overlay
+$overlay.append($btnExit);
+//Add overlay to body
+$("body").append($overlay);
+
+//Prevent Default and capture image to show
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  $(this).addClass("selected");
+  //Show image on overlay
+  $image.attr("src", imageLocation);
+  //Show the overlay
+  $overlay.show();
+  //Finding caption for clicked image
+  var $captionLocation = $(this).find("p.caption").text();
+  //Dispalays caption under image
+  $caption.text($captionLocation);
+
+
+});
+
+//When overlay is clicked
+$btnExit.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+  $("a").removeClass("selected");
+
+});
+
+$btnPrev.click(function() {
+
+ });

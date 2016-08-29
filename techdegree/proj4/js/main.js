aa5cb2fde41b6231ac0d859866064c07cec36dce
Russell Sawyer <rsawyer05@yahoo.com>
2016-08-28 19:08:35 -0500

    fix keyboard nav

--- techdegree/proj4/js/main.js
+++ techdegree/proj4/js/main.js
@@ -1,9 +1,9 @@
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $("<p></p>");
-var $btnPrev = $('<button id="btnPrev"> < </button>');
-var $btnNext = $('<button id="btnNext"> > </button>')
-var $btnExit = $('<button id="btnExit"> x </button>')
+var $btnPrev = $('<button id="btnPrev" type="button"> < </button>');
+var $btnNext = $('<button id="btnNext" type="button"> > </button>')
+var $btnExit = $('<button id="btnExit" type="button"> x </button>')
 //Add image to overlay
 $overlay.append($image);
 //Add caption to overlay
@@ -18,21 +18,21 @@ $overlay.append($btnExit);
 $("body").append($overlay);
 
 //Prevent Default and capture image to show
-$("#imageGallery div.image-wrapper").on("click", function(event){
+$("div.image-wrapper").on("click", function(event){
   event.preventDefault();
   var imageLocation = $(this).find("a").attr("href");
   //Show image on overlay
   $image.attr("src", imageLocation);
   //Remove class active from previous active item
-  $("#imageGallery div.active").removeClass("active");
+  $("div.active").removeClass("active");
   //Adds active class to active image
   $(this).addClass("active");
-  //Show the overlay
-  $overlay.show();
   //Finding caption for clicked image
   var $captionLocation = $(this).find("p.caption").text();
   //Dispalays caption under image
   $caption.text($captionLocation);
+  //Show the overlay
+  $overlay.show();
 
 
 
@@ -43,24 +43,26 @@ $btnExit.on("click", function(){
   //Hide the overlay
   $overlay.hide();
   //Remove class "active"
-  $("#imageGallery a.active").removeClass("active");
+  $("div.active").removeClass("active");
 
 });
 
 
-$("#overlay").on("keydown", function(event){
 
+$(document).on("keydown", function(event) {
+
+  if($("#overlay").css("display") !== 'none') {
     //Left
-  if(event.which == "37") {
-      navigate(-1);
-      console.log(event.which);
-    //Right
-  } else if(event.which == "39") {
-      navigate(1);
-      console.log(event.which);
+    if(event.which == "37") {
+        navigate(-1);
+      //Right
+    } else if(event.which == "39") {
+        navigate(1);
+    }
   }
 
 });
+
 
 //Click based navigation
 $btnPrev.on("click", function(){
@@ -74,8 +76,8 @@ $btnNext.on("click", function(){
 
 function navigate(direction){
   if(direction == -1) {  // left
-    $("#imageGallery div.active").prev().find(".image a").trigger("click");
+    $("div.active").prev().find(".image a").trigger("click");
   } else if (direction == 1) {  //right
-    $("#imageGallery div.active").next().find(".image a").trigger("click");
+    $("div.active").next().find(".image a").trigger("click");
   }
 }

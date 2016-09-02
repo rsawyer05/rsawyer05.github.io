4297feb6635c6b3e3544baa5f2eaf511a326ca5f
Russell Sawyer <rsawyer05@yahoo.com>
2016-09-01 23:07:35 -0500

    added search feature

--- techdegree/proj4/js/main.js
+++ techdegree/proj4/js/main.js
@@ -7,8 +7,7 @@ var $btnNext = $('<button id="btnNext" type="button"> > </button>');
 var $btnExit = $('<button id="btnExit" type="button"> x </button>');
 //Add image to overlay
 $overlay.append($image);
-
-
+//Add iframe to overlay
 $overlay.append($iframe);
 //Add caption to overlay
 $overlay.append($caption);
@@ -25,32 +24,31 @@ $("body").append($overlay);
 $("div.image-wrapper").on("click", function(event){
   event.preventDefault();
 
-  var imageLocation = $(this).find("a").attr("href");
+  var $imageLocation = $(this).find("a").attr("href");
 
-
-  var imageType = $(this).find("a").attr("class");
-  console.log(imageType);
-
-  if(imageType == "video"){
-    $iframe.attr("src", imageLocation);
-    $iframe.show();
-    $image.hide();
-
-  } else if(imageType == "image"){
-    $image.show();
-    $iframe.hide();
-    //Show image on overlay
-    $image.attr("src", imageLocation);
-  }
-
-  var $captionLocation = $(this).find(".caption").text();
-  $caption.text($captionLocation);
+  var $imageType = $(this).find("a").attr("class");
 
   //Remove class active from previous active item
   $("div.active").removeClass("active");
   //Adds active class to active image
   $(this).addClass("active");
+  //iframe display
+  if($imageType == "video"){
+    $iframe.attr("src", $imageLocation);
+    $iframe.show();
+    $image.hide();
 
+    //image display
+  } else if($imageType == "image"){
+    $image.attr("src", $imageLocation);
+    $image.show();
+    $iframe.hide();
+
+  }
+
+  //Caption display
+  var $captionLocation = $(this).find(".caption").text();
+  $caption.text($captionLocation);
 
   //Show the overlay
   $overlay.delay(200).fadeIn(400);
@@ -96,8 +94,31 @@ $btnNext.on("click", function(){
 //Navigation Magic .prev and .next
 function navigate(direction){
   if(direction == -1) {  // left
-    $("div.active").prev().find("a").trigger("click");
+    $("div.active").prev(".image-wrapper").find("a").trigger("click");
+
   } else if (direction == 1) {  //right
-    $("div.active").next().find("a").trigger("click");
-  }
+    $("div.active").next(".image-wrapper").find("a").trigger("click");
+    }
 }
+
+//Search box filter
+$("#searchbox").on("keyup", function() {
+
+  var $filter = $(this).val();
+  var count = 0;
+
+  $("div .caption").each(function() {
+    if($(this).text().search(new RegExp($filter, "i")) < 0) {
+      var $pic = $(this).parents(".image-wrapper");
+      $pic.fadeOut();
+
+    } else {
+      $(this).parents(".image-wrapper").fadeIn();
+      count++;
+    }
+  });
+
+  var numberItems = count;
+  $("#filter-count").text("Number of Pictures = " + count);
+
+});
